6f961ddca64769c3d4e069db41d2e6b05d4b6d76
Phillip Schichtel <phillip@schich.tel>
2019-12-15 21:01:17 +0100

    consolidated profile scripts

--- bashrc.local.d/020-terminal.sh
+++ bashrc.local.d/020-terminal.sh
@@ -18,10 +18,19 @@ fi
 set horizontal-scroll-mode on
 
 # make less more friendly for non-text input files, see lesspipe(1)
-[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+if [ -x /usr/bin/lesspipe ]
+then
+    eval "$(SHELL=/bin/sh lesspipe)"
+fi
 
 # enable color support of ls and also add handy aliases
-if [ -x /usr/bin/dircolors ]; then
+if [ -x /usr/bin/dircolors ]
+then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+fi
+
+if tty | grep -q tty
+then
+    export TMOUT=300
 fi
 
--- bashrc.local.d/030-timeout.sh
+++
@@ -1,5 +0,0 @@
-if tty | grep -q tty
-then
-    export TMOUT=300
-fi
-
