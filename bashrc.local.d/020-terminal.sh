9bd9fec6430bb8f1a9e2e88576a399e176455ac8
Phillip Schichtel <phillip@schich.tel>
2019-12-30 02:29:48 +0100

    added the bash.bashrc

--- bashrc.local.d/020-terminal.sh
+++ bashrc.local.d/020-terminal.sh
@@ -8,7 +8,7 @@ export HISTIGNORE='reboot:poweroff:exit:logout:clear'
 shopt -s histappend
 
 # update the values of LINES and COLUMNS.
-shopt -s checkwinsize
+[[ $DISPLAY ]] && shopt -s checkwinsize
 
 # If set, the pattern "**" used in a pathname expansion context will
 # match all files and zero or more directories and subdirectories.
