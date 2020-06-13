5e964054b45093993f07cecbbeeea28ec1c61fd0
Phillip Schichtel <phillip@schich.tel>
2020-06-13 23:33:46 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -eu
+
+here="$(dirname "${BASH_SOURCE[0]}")"
+
+sources=("bash.bashrc" "bashrc.local" "bashrc.local.d" "inputrc.local")
+
+for src in "${sources[@]}"
+do
+    sudo cp -va "${here}/${src}" /etc/
+done
+
